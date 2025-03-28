9e9969a6009949c0d647ed9019d81e6ab8bcf182
Marc0sRR <marcos.rama.1994@gmail.com>
2025-03-28 12:41:35 +0100

    Initial creation of interface

---
+++ main.py
@@ -0,0 +1,16 @@
+import customtkinter as ctk
+from viewInterface import MainInterface
+
+ctk.set_appearance_mode("dark")
+# Create main window
+root = ctk.CTk()
+
+# Create instance for main interface
+interface = MainInterface(root)
+
+# Configure size and title
+root.geometry("600x400")
+root.title("Treasury control")
+
+# Start app
+root.mainloop()
